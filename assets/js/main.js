deaaac8e214fac67184fb16e1696acf0996d2739
ckleo Siu <67173226+ckleo-design@users.noreply.github.com>
2024-09-25 22:46:00 +0900

    base

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+function PageTopAnime() {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 200) {
+        $('#page-top').removeClass('DownMove');
+        $('#page-top').addClass('UpMove');
+    } else {
+        if ($('#page-top').hasClass('UpMove')) {
+            $('#page-top').removeClass('UpMove');
+            $('#page-top').addClass('DownMove');
+        }
+    }
+}
+
+$(window).scroll(function () {
+    PageTopAnime(); /* スクロールした際の動きの関数を呼ぶ*/
+});
+
+$(window).on('load', function () {
+    PageTopAnime(); /* スクロールした際の動きの関数を呼ぶ*/
+});
+
+$('#page-top a').click(function () {
+    $('body,html').animate({
+        scrollTop: 0 //ページトップまでスクロール
+    }, 2500); //ページトップスクロールの速さ。数字が大きいほど遅くなる
+    return false; //リンク自体の無効化
+});
+
+$(".js-support-title").click(function () {
+    $(this).next().toggle("slow");
+});
+
+$(".openbtn1").click(function () {
+    $(this).toggleClass('active');
+    $(".header__inner__nav").toggleClass('active');
+});
