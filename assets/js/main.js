4982f346e5579b03fa7b7d453b60a4e1c6aa87df
ckleo Siu <67173226+ckleo-design@users.noreply.github.com>
2024-09-30 10:10:28 +0900

    finial up
    bug fixed
    GA
    tinyPNG

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,39 @@
 function PageTopAnime() {
-    var scroll = $(window).scrollTop();
+    var scroll = $(window).scrollTop()
     if (scroll >= 200) {
-        $('#page-top').removeClass('DownMove');
-        $('#page-top').addClass('UpMove');
+        $('.totop').removeClass('DownMove')
+        $('.totop').addClass('UpMove')
     } else {
-        if ($('#page-top').hasClass('UpMove')) {
-            $('#page-top').removeClass('UpMove');
-            $('#page-top').addClass('DownMove');
+        if ($('.totop').hasClass('UpMove')) {
+            $('.totop').removeClass('UpMove')
+            $('.totop').addClass('DownMove')
         }
     }
 }
 
 $(window).scroll(function () {
-    PageTopAnime(); /* スクロールした際の動きの関数を呼ぶ*/
-});
+    PageTopAnime() /* スクロールした際の動きの関数を呼ぶ*/
+})
 
 $(window).on('load', function () {
-    PageTopAnime(); /* スクロールした際の動きの関数を呼ぶ*/
-});
+    PageTopAnime() /* スクロールした際の動きの関数を呼ぶ*/
+})
 
-$('#page-top a').click(function () {
-    $('body,html').animate({
-        scrollTop: 0 //ページトップまでスクロール
-    }, 2500); //ページトップスクロールの速さ。数字が大きいほど遅くなる
-    return false; //リンク自体の無効化
-});
+$('.totop').click(function () {
+    $('body,html').animate(
+        {
+            scrollTop: 0 //ページトップまでスクロール
+        },
+        2500
+    ) //ページトップスクロールの速さ。数字が大きいほど遅くなる
+    return false //リンク自体の無効化
+})
 
-$(".js-support-title").click(function () {
-    $(this).next().toggle("slow");
-});
+$('.js-support-title').click(function () {
+    $(this).next().toggle('slow')
+})
 
-$(".openbtn1").click(function () {
-    $(this).toggleClass('active');
-    $(".header__inner__nav").toggleClass('active');
-});
+$('.openbtn1').click(function () {
+    $(this).toggleClass('active')
+    $('.header__inner__nav').toggleClass('active')
+})
